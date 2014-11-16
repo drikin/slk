79ed7cf219f1f7d1e6d86c65b3c171135e526d94
Koh Aoki <drikin@gmail.com>
2014-11-16 11:41:02 -0800

    modify as Slack app

--- stage.js
+++ stage.js
@@ -1,46 +1,12 @@
 (function() {
+  // Load native UI library
   var gui = require('nw.gui');
-  var iframe = document.querySelector('iframe');
+  // Get the current window
+  var win = gui.Window.get();
 
-  var handleClick = function(e) {
-    var checkForLink = function(el) {
-      var openLink = function(url) {
-        gui.Shell.openExternal(url);
-        e.preventDefault();
-      };
-
-      if (el.nodeName.toLowerCase() === 'a') {
-        var href = el.getAttribute('href');
-        if (href !== null) {
-          var middleButton = e.which === 2;
-          if (middleButton ||
-              (el.target === '_blank' &&
-               el.rel !== 'user' &&
-               el.rel !== 'mediaPreview')) {
-            openLink(href);
-          }
-
-          console.log(href, middleButton, el.target, el.rel);
-        }
-      } else if ((p = el.parentElement) !== null) {
-        checkForLink(p);
-      }
-    };
-
-    checkForLink(e.target);
-  };
-  
-  var init = function() {
-    if (iframe &&
-        iframe.contentWindow &&
-        iframe.contentWindow.document &&
-        iframe.contentWindow.document.body &&
-        iframe.contentWindow.document.body.innerHTML) {
-     iframe.contentWindow.document.body.addEventListener('click', handleClick, false);
-    } else {
-      setTimeout(init, 100);
-    }
-  };
-  
-  init();
+  win.on('new-win-policy', function(frame, url, policy){
+    policy.ignore();
+    gui.Shell.openExternal(url);
+    //policy.forceNewWindow();
+  });
 }).apply(this);
