429e640a3af3cb4f11f401aa47500ed85997734f
Koh Aoki <drikin@gmail.com>
2014-11-17 08:33:09 -0800

    workaround for Japanese font issue

--- stage.js
+++ stage.js
@@ -1,12 +1,19 @@
 (function() {
-  // Load native UI library
+  var $ = require('jquery');
   var gui = require('nw.gui');
+
   // Get the current window
   var win = gui.Window.get();
 
-  win.on('new-win-policy', function(frame, url, policy){
+  win.on('new-win-policy', function(frame, url, policy) {
     policy.ignore();
     gui.Shell.openExternal(url);
     //policy.forceNewWindow();
+  });
+
+  // FIXME: very hacky way to forcefully update font-family
+  win.on('document-end', function() {
+    var iframe = window.frames["main"];
+    $(iframe.contentDocument.head).contents().append("<style>.message{font-family:'Lucida Grande', 'Hiragino Kaku Gothic ProN', 'ヒラギノ角ゴ ProN W3',  Meiryo, メイリオ, sans-serif !important;}</style>");
   });
 }).apply(this);
