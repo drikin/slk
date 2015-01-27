bc3a8c1acee5c20118f09321f4fb9bff5e95ac99
Koh Aoki <Kohichi.Aoki@am.sony.com>
2015-01-27 13:07:22 -0800

    override Notification object for avoiding some issues
    One biggest issue is crashing by large message on Win7

--- src/stage.js
+++ src/stage.js
@@ -44,10 +44,23 @@
         win.setBadgeLabel('');
       }
     }
-    iwindow. Notification.prototype.show = function(title, option) {
+
+    // override Notification object for avoiding some issues
+    var _notification = Notification;
+    _notification.prototype.show = function() {
       if (enableNotification) {
         win.setBadgeLabel(++badgeLabelNumber);
       }
     }
+
+    iwindow.Notification = function(title, options) {
+        if (options.body) {
+            options.body = options.body.slice(0,50);
+        }
+        return new _notification(title, options);
+    }
+    iwindow.Notification.permission = _notification.permission;
+    iwindow.Notification.requestPermission = _notification.requestPermission;
+
   });
 }).apply(this);
